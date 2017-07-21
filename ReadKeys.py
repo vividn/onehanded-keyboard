b56e07a8cad9964a6a9b9e36d4e220e4d3d92e45
Nate Faber <nathaniel@njfaber.com>
2017-07-21 16:52:50 -0500

    Make basic script that detects all keystrokes from the numpad

---
+++ ReadKeys.py
@@ -0,0 +1,28 @@
+import evdev
+from evdev import ecodes
+from select import select
+import code
+import time
+
+deviceName = "DaKai 2.4G RX"
+
+kbds = []
+# Load all the relevant devices
+allDevices = [evdev.InputDevice(dv) for dv in evdev.list_devices()]
+for dev in allDevices:
+    if deviceName in dev.name:
+        kbds.append(dev)
+
+        # Make this program the exclusive handler of this device
+        dev.grab()
+
+# Create a dictionary that references the input device by its file descriptor
+kbds = {dev.fd: dev for dev in kbds}
+
+# Now continuously loop and read in input events
+while True:
+    r, w, x = select(kbds, [], [])
+    for fd in r:
+        for event in kbds[fd].read():
+            if event.type == 1 and event.value < 2:
+                print(event)
