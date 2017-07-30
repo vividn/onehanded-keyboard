6a9774c3eb11a9f3a17b24f8fdd469d0d14156a9
Nate Faber <nathaniel@njfaber.com>
2017-07-30 00:30:45 -0500

    Process modifier keys too

--- inputEventCreator.py
+++ inputEventCreator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import evdev
+import time
 from evdev import UInput, ecodes as e
 import re
 from ast import literal_eval as make_tuple
@@ -50,51 +51,66 @@ def process_queue_helper(queue):
 
     # Return if input queue is empty
     if nKeys == 0:
-        return ([],[])
+        return [],[]
 
     # If the top event on the queue is a key release action, just delete the entry.
     if queue[0]['state'] == 0:
-        coord_list = []
-        indices_to_delete = [0]
+        return [],[0]
 
-    # Normal keys (non modifiers)
-    elif queue[0]['coord'] not in modKeys and \
-            queue[0]['coord'] not in sModKeys:
+    # Get which type of key the first key is
+    if queue[0] in sModKeys:
+        keyType = 'SMOD'
+    elif queue[0] in modKeys:
+        keyType = 'MOD'
+    else:
+        keyType = 'NORMAL'
 
-        if nKeys >= 2:
+    if nKeys >= 2:
 
-            # If the second event is just releasing the button (no chord has occurred), return just that key
-            if queue[1]['coord'] == queue[0]['coord']:
-                if queue[1]['type'] == 1: warnings.warn("Two key down events on the same key occurred somehow")
+        # If the second event is just releasing the button (no chord has occurred)
+        if queue[1]['coord'] == queue[0]['coord']:
+
+            if keyType == 'SMOD' and time.time() - queue[0]['time'] <= smodStickTime:
+                # Wait for more input if a sticky modifier is pressed
+                coord_list = []
+                indices_to_delete = []
+
+            else:
+                # Return just key otherwise
                 coord_list = [queue[0]['coord']]
                 indices_to_delete = [0,1]
 
-            # If the next key occurs quickly, treat the input as a key chord
-            elif queue[1]['time'] - queue[0]['time'] <= config.chordTime:
-                # Call process_queue_helper recursively to allow for arbitrarily big key chords
-                [sub_coords, sub_inds] = process_queue_helper(queue[1:-1])
+        # If the next key occurs quickly or first key is a modifier, treat the input as a key chord
+        elif queue[1]['time'] - queue[0]['time'] <= config.chordTime or keyType != 'NORMAL':
+            # Call process_queue_helper recursively to allow for arbitrarily big key chords
+            [sub_coords, sub_inds] = process_queue_helper(queue[1:-1])
 
-                coord_list = [queue[0]['coord']] + sub_coords
-                indices_to_delete = [0] + [i + 1 for i in sub_coords]
+            coord_list = [queue[0]['coord']] + sub_coords
+            indices_to_delete = [0] + [i + 1 for i in sub_coords]
 
-            # Otherwise, just output the key by itself
-            else:
-                coord_list = [queue[0]['coord']]
-                indices_to_delete = [0]
-
-        # If enough time has passed where a key chord is not possible, just return the key
+        # Otherwise, just output the key by itself
         else:
             coord_list = [queue[0]['coord']]
             indices_to_delete = [0]
 
-    # First key is a mod or smod key being pressed down
-    elif
+    else:
+        # Only one event in queue
 
-    return (coord_list, indices_to_delete)
+        # Output the key if NORMAL key and the time for key chords has passed
+        if keyType == 'NORMAL' and time.time() - queue[0][time] > config.chordTime:
+            coord_list = [queue[0]['coord']]
+            indices_to_delete = [0]
+
+        else:
+            # Otherwise, just wait for more key presses to come in
+            coord_list = []
+            indices_to_delete = []
+
+    return coord_list, indices_to_delete
 
 
 def write_from_coords(coord_list):
-    ## Output the key stroke events to the computer
+    # Output the key stroke events to the computer
 
     # Turn the coordinates into their appropriate list of key names to be outputted
     ecode_list = get_ecode_list(coord_list)
