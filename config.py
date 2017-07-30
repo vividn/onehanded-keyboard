2efb5819aefc53f6036d3a023a05ebb9f9373a65
Nate Faber <nathaniel@njfaber.com>
2017-07-30 00:01:41 -0500

    Switch meaning of SMOD to mean sticky modifier

--- config.py
+++ config.py
@@ -5,12 +5,5 @@
 # Onset of second key must precede offset of first key
 chordTime = 0.1
 
-# This setting enables a normal modifier key to be pressed and released to be activated, rather than continuously held
-# Necessary if all three key chord combinations don't work with the hardware
-stickyMod = True
-
-# Similar setting for the special mod keys. The way I have it currently implemented the special mod keys are only used with one key at a time, so this isn't needed
-stickySpecial = False
-
 # How long a sticky key is held for
-modStickTime = 0.75
+smodStickTime = 0.75
