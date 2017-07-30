7e32f86da12149354c5db58aee60e9ec1f8e5970
Nate Faber <nathaniel@njfaber.com>
2017-07-29 23:51:16 -0500

    Process normal keys and key chords

--- inputEventCreator.py
+++ inputEventCreator.py
@@ -6,6 +6,8 @@ import re
 from ast import literal_eval as make_tuple
 from itertools import permutations
 import os
+import config
+import warnings
 
 from keymaps import modKeys, sModKeys, outputMap, inputMap
 
@@ -34,7 +36,7 @@ def process_queue():
     # Sort the queue by time of events
     inputQueue = sorted(inputQueue, key=itemgetter('time'))
 
-    while length(inputQueue) > 0:
+    while len(inputQueue) > 0:
         (coord_list, indices_to_delete) = process_queue_helper(inputQueue)
         write_from_coords(coord_list)
 
@@ -44,11 +46,48 @@ def process_queue():
 
 def process_queue_helper(queue):
 
+    nKeys = len(queue)
+
+    # Return if input queue is empty
+    if nKeys == 0:
+        return ([],[])
+
     # If the top event on the queue is a key release action, just delete the entry.
     if queue[0]['state'] == 0:
         coord_list = []
+        indices_to_delete = [0]
 
-    # S
+    # Normal keys (non modifiers)
+    elif queue[0]['coord'] not in modKeys and \
+            queue[0]['coord'] not in sModKeys:
+
+        if nKeys >= 2:
+
+            # If the second event is just releasing the button (no chord has occurred), return just that key
+            if queue[1]['coord'] == queue[0]['coord']:
+                if queue[1]['type'] == 1: warnings.warn("Two key down events on the same key occurred somehow")
+                coord_list = [queue[0]['coord']]
+                indices_to_delete = [0,1]
+
+            # If the next key occurs quickly, treat the input as a key chord
+            elif queue[1]['time'] - queue[0]['time'] <= config.chordTime:
+                # Call process_queue_helper recursively to allow for arbitrarily big key chords
+                [sub_coords, sub_inds] = process_queue_helper(queue[1:-1])
+
+                coord_list = [queue[0]['coord']] + sub_coords
+                indices_to_delete = [0] + [i + 1 for i in sub_coords]
+
+            # Otherwise, just output the key by itself
+            else:
+                coord_list = [queue[0]['coord']]
+                indices_to_delete = [0]
+
+        # If enough time has passed where a key chord is not possible, just return the key
+        else:
+            coord_list = [queue[0]['coord']]
+            indices_to_delete = [0]
+
+    # First key is a mod or smod key being pressed down
     elif
 
     return (coord_list, indices_to_delete)
